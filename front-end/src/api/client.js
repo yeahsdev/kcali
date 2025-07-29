ad0669580fb5644f3cb63c9a31c33942dced9057
yeahsdev <iyeahs71@gmail>
2025-07-29 17:30:13 +0900

    update initial login setting

---
+++ front-end/src/api/client.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// axios 인스턴스 생성
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8001/api/',
+});
+
+// 요청 인터셉터 (모든 요청에 JWT 토큰 자동 추가)
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ 추가
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter> {/* ✅ Router 감싸기 */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
