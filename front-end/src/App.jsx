b7e0567b23939835300baca7c22bcfbd9c5a40eb
yeahsdev <iyeahs71@gmail>
2025-07-30 14:30:18 +0900

    Apply Index style

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
 // 우리가 만들 페이지 컴포넌트들을 import 합니다.
+import HomePage from './pages/HomePage'; // 👈 HomePage 추가
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import SignupPage from './pages/SignupPage'; // 👈 1. 이 줄을 추가하세요.
@@ -17,7 +18,7 @@ function App() {
 
       {/* URL 경로에 따라 어떤 페이지를 보여줄지 결정하는 부분입니다. */}
       <Routes>
-        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/" element={<HomePage />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} /> 
         <Route path="/dashboard" element={<DashboardPage />} />
